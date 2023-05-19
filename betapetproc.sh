7566111a6692011f35d8a8b8e1b4de7971d35feb
Norah (Jo) Vii <70073104+norahvii@users.noreply.github.com>
2023-05-19 13:48:16 -0500

    Add files via upload

---
+++ betapetproc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function move_PET_files() {
+    fs_mri_path=`find INPUT/FreeSurfer/* -name "mri" -type d | head -n 1`
+    fs_path=`echo ${fs_mri_path} | sed -e 's/\/mri//g'`; echo $fs_path
+    mkdir -p ../../data/input/FreeSurfer
+    mv ${fs_path}/* ../../data/input/FreeSurfer/
+}
+
+move_PET_files
+echo "Finished moving FreeSurfer folders."
+
+
+function move_PET_files() {
+    find ./INPUT/PET -type f -name "*.dcm" -exec mkdir -p ../../data/input/PET/ \; -exec mv {} ../../data/input/PET/ \;
+}
+
+move_PET_files
+echo "Finished moving PET files."
+
+
+# Change to the output directory
+pushd ./OUTPUT/
+
+# Loop over all .params files in the input directory
+find /data/input/params -name "*.params" | while read params_file; do
+    # Generate the output log file path based on the name of the input file
+    log_file="$(basename "$params_file" .params).petlog"
+
+    # Run petproc with the current parameter file and redirect output to the log file
+    petproc "$params_file" > "$log_file"
+done
+
+# Return to the previous directory
+popd
+
+echo "petproc Complete!"
